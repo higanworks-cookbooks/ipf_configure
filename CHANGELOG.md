bd66a5177a234cd26e7e76170931e1dde506b7d9
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-29 16:49:05 +0900

    bump 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 This file is used to list changes made in each version of ipf_configure.
 
-## 0.1.0:
+## 0.2.0:
 
 * Initial release of ipf_configure
 
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sawanoboriyu@higanworks.com'
 license          ''
 description      'Installs/Configures ipf_configure'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends          'ipf'  # modcloth-cookbooks@github
 supports         'smartos'
