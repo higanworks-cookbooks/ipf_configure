803ff09a3167a6e5cdf944f82248698e38629b4d
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-29 16:58:17 +0900

    define license for community upload

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'ipf_configure'
 maintainer       'HiganWorks LLC.'
 maintainer_email 'sawanoboriyu@higanworks.com'
-license          ''
+license          'apache2'
 description      'Installs/Configures ipf_configure'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
