c94a36253bef9a50dcb61476bbdf052c07219c16
sawanoboly <sawanoboriyu@higanworks.com>
2013-05-29 16:42:54 +0900

    initilal commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'ipf_configure'
+maintainer       'HiganWorks LLC.'
+maintainer_email 'sawanoboriyu@higanworks.com'
+license          ''
+description      'Installs/Configures ipf_configure'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'ipf'  # modcloth-cookbooks@github
+supports         'smartos'
---
+++ recipes/example.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: ipf_configure
+# Recipe:: example
+#
+# Copyright 2013, HiganWorks LLC.
+#
+
+
+node.set['ipf']['rules']['pass_in'] << 'any'
+
